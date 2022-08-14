a1587b9c0337f1e28546275ff35398009a1be9c6
ElissonDeSous <94570280+ElissonDeSous@users.noreply.github.com>
2022-08-13 21:17:39 -0300

    epic games clone

---
+++ epic_games/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <header>
+           <div class = "icone"> <a href=""> <img src="img/icone.png" height = "50px" alt=""></a>
+           </div>
+            <nav>
+                <ul>
+                    <li><a href="">STORE</a></li>
+                    <li><a href="">PERGUNTAS FREQUENTE</a></li>
+                    <li><a href="">AJUDA</a></li>
+                    <li><a href="">UNREAL ENGINE</a></li>
+                </ul>
+            </nav>
+                <div id = "topo">
+                <a class = "link-entrar" href="login.php">ENTRAR</a>
+              <div class= "link-baixar"><a href="">BAIXAR AGORA</a></div>
+                 </div>
+        
+        </header>
+
+        <section>
+            <div id = "menu2">
+              <ul>
+                <input type="search" name="" id="input">
+                <li><a href="">Navegar</a></li>
+                <li><a href="">Novidades</a></li>
+              </ul>
+            </div>
+           
+            <div id = "conteudo">
+                 <div class = "destaque">
+                      <img src="img/rocket-league.jfif" height = "500px" alt="">
+                 </div>
+                 <h2>Jogos em promoção</h2>
+                 <div class = "promo">
+                    <div class = "jogo1">
+                         <img src="img/gta 5.jfif " alt="">
+                         <p>GTA 5 99 R$</p>
+                    </div>
+                    <div class = "jogo2">
+                         <img src="img/cyber.png" width = "170px" height = "250px" alt="">
+                         <p>Cyberpunk 2077 40 R$</p>
+                    </div>
+                    <div class = "jogo3">
+                         <img src="img/watch-dogs.jpg" width = "170px" height = "250px" alt="">
+                         <P>watch dogs 30 R$</P>
+                    </div>
+                 </div>
+            </div>
+        </section>
+        <footer>
+            &copy; todos direitos reservados a EPIC GAMES
+        </footer>
+    </main>
+</body>
+</html>
